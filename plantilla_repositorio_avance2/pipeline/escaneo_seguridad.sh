00f0746dd764f0db02c7f3baaf930e93aea2b2bc
EC2 Default User <ec2-user@ip-172-31-30-1.ec2.internal>
2026-09-18 03:59:43 +0000

    entrega con pipeline bloqueando y evidencias

--- plantilla_repositorio_avance2/pipeline/escaneo_seguridad.sh
+++ plantilla_repositorio_avance2/pipeline/escaneo_seguridad.sh
@@ -1,1 +1,3 @@
-trivy image --exit-code 1 --severity CRITICAL $IMAGE_NAME
+#!/bin/bash
+echo "Escaneando el proyecto en busca de vulnerabilidades y secretos..."
+trivy fs --scanners secret,vuln --exit-code 1 --severity CRITICAL,HIGH .
